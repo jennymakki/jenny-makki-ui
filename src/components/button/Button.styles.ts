2ae156b0f4ab035654b68e25b097720eded43cc9
jennymakki <jjennyeklundd@hotmail.com>
2026-04-19 11:35:28 +0200

    added styles for the button

---
+++ src/components/button/Button.styles.ts
@@ -0,0 +1,14 @@
+export const buttonStyles = {
+    base: "rounded font-semibold transition",
+
+    variants: {
+        primary: "bg-blue-500 text-white hover:bg-bllue-600",
+        secondary: "bg-gray-200 text-black hover:bg-gray-300",
+    },
+
+    sizes: {
+        small: "px-2 py-1 text-sm",
+        medium: "px-4 py-2",
+        large: "px-6 py-3 text-lg",
+    },
+};
